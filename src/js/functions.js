fb57cd4bcb083f035e4bd63f8fd0fd2fc6037681
ira <bakum21@ukr.net>
2016-05-20 10:39:22 +0300

    git init

---
+++ src/js/functions.js
@@ -0,0 +1,34 @@
+jQuery(document).ready(function() {
+
+    //form validate
+    jQuery("form").validate({
+
+        rules:{
+            name:{
+                required: true,
+                minlength: 2
+            },
+            phone:{
+                required: true,
+                digits: true
+            },
+            email:{
+                required: true,
+                email: true
+            },
+            address:{
+                required: true
+            }
+        }
+    });
+
+
+
+    jQuery(window).load(function() {
+        
+
+    });
+
+
+
+});
